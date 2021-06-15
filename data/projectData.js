730326c57af8c508f4c0b521346258e0172afebb
Heather Peterson <hpeterson462@gmail.com>
2021-06-14 17:21:47 -0700

    mock project data

---
+++ data/projectData.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    title: 'Project 1',
+    point_one: 'Hook',
+    point_two: 'Plot Turn 1',
+    point_three: 'Pinch 1',
+    point_four: 'Midpoint',
+    point_five: 'Pinch 2',
+    point_six: 'Plot Turn 2',
+    point_seven: 'Resolution',
+    owner_id: '1'
+  }
+];
